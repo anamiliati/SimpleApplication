1066b76783630215579d03eb1c674c927a8a2e36
unknown <you@example.com>
2025-03-13 19:57:51 +0000

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,13 @@
 # Enable error handling
 set -e
 
-echo "Configuring SSH keys..."
+echo "Configurando chaves SSH..."
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-# Create private key from CircleCI environment variable
-echo "$PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-
-# Add the server key to known_hosts
-echo "$SERVER_KEY" > ~/.ssh/known_hosts
+# Create private and server key from CircleCI environment variable
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 
 echo "Deploying to the server..."
 ssh -i ~/.ssh/id_rsa ana@34.245.108.72 "C:\Users\Ana\Desktop\CA_DEV\SimpleApplication && git pull origin main && npm starth"
-
