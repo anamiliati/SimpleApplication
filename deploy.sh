51c650ae87141e5c5738d0c9aba3391fef7660dc
unknown <you@example.com>
2025-03-14 18:58:55 +0000

    added continuous delivery

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
